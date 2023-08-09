63a5f3bab6fdf004338399f4d763bfe51473d992
Sam Foreman <saforem2@gmail.com>
2023-08-09 13:30:26 -0500

    Update `ALCF/{sweep_args_theta_multinode.sh,benchmark_train.sh}`

--- ALCF/benchmark_train.sh
+++ ALCF/benchmark_train.sh
@@ -2,15 +2,7 @@
 
 TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
 # DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -LP)
-
-SOURCE=${BASH_SOURCE[0]}
-while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  SOURCE=$(readlink "$SOURCE")
-  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-
+#
 
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #┃ Make sure we're not already running; if so, exit here ┃
@@ -21,18 +13,59 @@ if [ -n "${PIDS}" ]; then
   exit 1
 fi
 
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+
+function sourceFile() {
+  FILE="$1"
+  echo "source-ing ${FILE}"
+  if [[ -f "${FILE}" ]]; then
+    # shellcheck source="${FILE}"
+    source "${FILE}"
+  else
+    echo "ERROR: UNABLE TO SOURCE ${FILE}"
+  fi
+}
+
+SETUP_FILE="${DIR}/setup.sh"
+MODEL_FILE="${DIR}/model.sh"
+ARGS_FILE="${DIR}/args.sh"
+LAUNCH_FILE="${DIR}/launch.sh"
+
+
+sourceFile "${SETUP_FILE}"
+sourceFile "${MODEL_FILE}"
+sourceFile "${ARGS_FILE}"
+sourceFile "${LAUNCH_FILE}"
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "My current script is: ${SCRIPT_DIR[0]}"
+
+NHOSTS=$(wc -l < "${COBALT_NODEFILE}")
+# NGPU_PER_HOST=8
+NGPU_PER_HOST=$(nvidia-smi -L | wc -l)
+PARALLEL_SIZE=$(( NHOSTS * NGPU_PER_HOST ))
+
+export MODEL_TYPE=${MODEL_TYPE:-"gpt"} # set bert or gpt
+export SP_TYPE=${SP_TYPE:-"megatron"} # set ds or megatron
+
+echo "+-----------------------------+"
+echo "| MODEL TYPE: ${MODEL_TYPE}"
+echo "| SP TYPE: ${SP_TYPE}"
+echo "+-----------------------------+"
+
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #┃ source ./launch.sh                       ┃
 #┃ which then sources ./{args.sh,setup.sh}  ┃
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-LAUNCH_FILE="${DIR}/launch.sh"
-if [[ -f "${LAUNCH_FILE}" ]]; then
-  echo "source-ing ${LAUNCH_FILE}"
-  # shellcheck source=./launch.sh
-  source "${LAUNCH_FILE}"
-else
-  echo "ERROR: UNABLE TO SOURCE ${LAUNCH_FILE}"
-fi
 
 export MODEL_TYPE=${MODEL_TYPE:-gpt}
 
@@ -42,7 +75,7 @@ setup
 TORCH_VERSION=$(python3 -c 'import torch; print(torch.__version__)')
 export TORCH_VERSION=$TORCH_VERSION
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-fullNode "$@"
+# fullNode "$@"
 # elasticDistributed "$@" 2>&1 &
 # elasticDistributed "$@"
 PID=$!
--- ALCF/sweep_args_theta_multinode.sh
+++ ALCF/sweep_args_theta_multinode.sh
@@ -2,18 +2,69 @@
 
 module load conda/2023-01-11
 conda activate base
-cd /home/czh5/seq/Megatron-DS-Benchmarking/ALCF
-source /home/czh5/seq/Megatron-DS-Benchmarking/venvs/thetaGPU/2023-01-11-deepspeed/bin/activate
+# cd /home/czh5/seq/Megatron-DS-Benchmarking/ALCF
+# source /home/czh5/seq/Megatron-DS-Benchmarking/venvs/thetaGPU/2023-01-11-deepspeed/bin/activate
 
-rm -rf /home/czh5/genome/Megatron-DeepSpeed/dataset/*.npy
-rm -rf /home/czh5/genome/Megatron-DeepSpeed/dataset/*.done
+# rm -rf /home/czh5/genome/Megatron-DeepSpeed/dataset/*.npy
+# rm -rf /home/czh5/genome/Megatron-DeepSpeed/dataset/*.done
+#
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+while [ -L "$SCRIPT_PATH" ]; do
+  SCRIPT_DIR="$(cd -P "$(dirname "$SCRIPT_PATH")" >/dev/null 2>&1 && pwd)"
+  SCRIPT_PATH="$(readlink "$SCRIPT_PATH")"
+  [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
+done
+SCRIPT_PATH="$(readlink -f "$SCRIPT_PATH")"
+SCRIPT_DIR="$(cd -P "$(dirname -- "$SCRIPT_PATH")" >/dev/null 2>&1 && pwd)"
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+
+function sourceFile() {
+  FILE="$1"
+  echo "source-ing ${FILE}"
+  if [[ -f "${FILE}" ]]; then
+    # shellcheck source="${FILE}"
+    source "${FILE}"
+  else
+    echo "ERROR: UNABLE TO SOURCE ${FILE}"
+  fi
+}
+
+SETUP_FILE="${DIR}/setup.sh"
+MODEL_FILE="${DIR}/model.sh"
+ARGS_FILE="${DIR}/args.sh"
+LAUNCH_FILE="${DIR}/launch.sh"
+
+
+sourceFile "${SETUP_FILE}"
+sourceFile "${MODEL_FILE}"
+sourceFile "${ARGS_FILE}"
+sourceFile "${LAUNCH_FILE}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "My current script is: ${SCRIPT_DIR[0]}"
 
-NHOSTS=$(wc -l < "${COBALT_NODEFILE}")
-NGPU_PER_HOST=8
-PARALLEL_SIZE=$((${NHOSTS}*${NGPU_PER_HOST}))
+
+if [[ $(hostname) == theta* ]]; then
+  HOSTFILE="${COBALT_NODEFILE}"
+elif [[ $(hostname) == x* ]]; then
+  HOSTFILE="${PBS_NODEFILE}"
+else
+  echo "Unexpected hostname $(hostname)"
+fi
+
+echo "Found hostfile: ${HOSTFILE}"
+
+NHOSTS=$(wc -l < "${HOSTFILE}")
+NGPU_PER_HOST=$(nvidia-smi -L | wc -l)
+PARALLEL_SIZE=$(( NHOSTS * NGPU_PER_HOST ))
 
 export MODEL_TYPE=${MODEL_TYPE:-"gpt"} # set bert or gpt
 export SP_TYPE=${SP_TYPE:-"megatron"} # set ds or megatron
